ace49616e1158452b63a385efc671a915ae62981
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2022-12-29 23:40:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def main(number: int):
 		services.zarubas_service(number)
 		services.youla_service(number)
 		services.yandex_service(number)
-		#services.ivi_service(number)
-		#services.kontur_service(number)
-		#services.vkusvill_service(number)
-		#services.x5id_service(number)
-		#services.medtochka_service(number)
-		#services.magnit_service(number)
-		#services.cg_service(number)
+		services.ivi_service(number)
+		services.kontur_service(number)
+		services.vkusvill_service(number)
+		services.x5id_service(number)
+		services.medtochka_service(number)
+		services.magnit_service(number)
+		services.cg_service(number)
 	except Exception as Error:	
-		print(Error)
+		pass
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
