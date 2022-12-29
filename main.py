bd8f696c981221beb11581aa8a1eded5acfb42d6
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2022-12-29 23:40:01 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import services
+import argparse
+
+	
+@services.circle_function
+def main(number: int):
+	try:
+		services.zarubas_service(number)
+		services.youla_service(number)
+		services.yandex_service(number)
+		#services.ivi_service(number)
+		#services.kontur_service(number)
+		#services.vkusvill_service(number)
+		#services.x5id_service(number)
+		#services.medtochka_service(number)
+		#services.magnit_service(number)
+		#services.cg_service(number)
+	except Exception as Error:	
+		print(Error)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--number', '-n',
+		action='store',
+		dest='number',
+		help='send number, example: 9000000000')
+	args = parser.parse_args()
+	main(args.number)
+		
