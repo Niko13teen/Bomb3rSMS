38099f9316aedcbe31c6d08eaf43bc3cbfb95b37
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2022-12-31 12:30:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import services
 import argparse
-from modules import *
+from modules import circle_function
 	
-@modules.circle_function
+@circle_function
 def main(number: int):
 	try:
 		services.zarubas_service(number)
